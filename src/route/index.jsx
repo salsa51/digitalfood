fd57cb2e4da057561c247ef8489f2f87c0f1174c
salsa51 <salsabela497@gmail.com>
2024-05-21 20:33:52 +0700

    Add index to version control

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -3,6 +3,7 @@ import NavLayout from "../components/Layouts";
 import LandingPage from "../pages/LandingPage";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import MarketPlace from "../pages/MarketPlace";
 
 const AppRouter = () => {
 
@@ -12,6 +13,7 @@ const AppRouter = () => {
       <Route path='/' element={<NavLayout><LandingPage /></NavLayout>} />      
       <Route path='SignIn' element={<SignIn />} />
       <Route path='SignUp' element={<SignUp />} />
+      <Route path='MarketPlace' element={<NavLayout><MarketPlace /></NavLayout>} />  
     </Routes>
 
   );
